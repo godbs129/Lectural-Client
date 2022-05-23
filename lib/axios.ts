9b688bb90fae31f17f939deabc70d0ef49dcff50
godbs129 <junhaeyoon@gmail.com>
2022-05-23 15:53:22 +0900

    asdf

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
-import { Token } from './token';
+import { AccessToken } from '../constants/token';
+import { requestHandler } from './requestHandler';
+import Token from './token';
 
 const customAxios = axios.create({
   baseURL: process.env.NEXT_PUBLIC_ENDPOINT,
   headers: {
-    authorization: `${Token().getToken('accessToken')}`,
+    authorization: `${Token.getToken(AccessToken) as string}`,
   },
 });
+
+customAxios.interceptors.response.use(requestHandler);
 
 export default customAxios;

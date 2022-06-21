cd2bc1adf6f01cc9013c7a4a9514bcd990e101aa
godbs129 <junhaeyoon@gmail.com>
2022-06-21 12:21:18 +0900

    ADD: 주제 요청 모달 추가

--- components/MainPage/MainPage.tsx
+++ components/MainPage/MainPage.tsx
@@ -1,9 +1,11 @@
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { isLectureModalOpen, lectureModalData } from '../../store/lectureStore';
+import { isRequestModalOpen } from '../../store/requestStore';
 import { userAtom } from '../../store/userStore';
 import { LectureData } from '../../types/Lecture';
 import LectureModal from '../Common/Modal/Lecture/LectureModal';
 import Modal from '../Common/Modal/Modal';
+import RequestModal from '../Common/Modal/Request/RequestModal';
 import Header from '../Header/Header';
 import List from '../List/List';
 import Notice from '../Notice/Notice';
@@ -13,12 +15,14 @@ import * as S from './MainPage.style';
 
 const MainPage = (): JSX.Element => {
   const [isLectureOpen, setIsLectureOpen] = useRecoilState(isLectureModalOpen);
+  const [isRequestOpen, setIsRequestOpen] = useRecoilState(isRequestModalOpen);
   const lecture = useRecoilValue(lectureModalData);
   const user = useRecoilValue(userAtom);
 
   return (
     <>
-      {isLectureOpen && LectureModal(lecture, user)}
+      {isLectureOpen && <LectureModal lecture={lecture} user={user} />}
+      {isRequestOpen && <RequestModal />}
       <Header />
       <Notice />
       <S.MainWrap>
